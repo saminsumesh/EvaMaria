854d8ce49e3a3012a698030e031bb3d28a1d0e15
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-01 16:46:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -586,7 +586,7 @@ async def auto_filter(client, message):
             )
         imdb = await get_poster(search) if IMDB else None
         if imdb and imdb.get('poster'):
-            await message.reply_photo(photo=imdb.get('poster'), caption=f"🇱🇷 Title: <a href={imdb['url']}>{imdb.get('title')}</a>\n\n🎭 Genres: {imdb.get('genres')}\n\n📆 Year: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n\n🤞 plot: {imdb.get('plot')}, reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=imdb.get('poster'), caption=f"🇱🇷 Title: <a href={imdb['url']}>{imdb.get('title')}</a>\n\n🎭 Genres: {imdb.get('genres')}\n\n📆 Year: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n\n🤞 plot: {imdb.get('plot')}", reply_markup=InlineKeyboardMarkup(btn))
         elif imdb:
             await message.reply_text(f"🇱🇷 Title: <a href={imdb['url']}>{imdb.get('title')}</a>\n\n🎭 Genres: {imdb.get('genres')}\n\n📆 Year: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n\n🤞 plot: {imdb.get('plot')} ", reply_markup=InlineKeyboardMarkup(btn))
         else:
