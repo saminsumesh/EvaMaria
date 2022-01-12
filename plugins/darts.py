b1cf6d4659de042072386749797d70b8ab115e96
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-01-12 10:25:02 +0530

    Update darts.py

--- plugins/darts.py
+++ plugins/darts.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
  
-EMOJI = "🎯" #use the emoji which supports animation
+D_EMOJI = "🎯" #use the emoji which supports animation
  
  
 @Client.on_message(filters.command("dart", "throw")) #change the command according to your emoji name
@@ -10,7 +10,7 @@ async def roll_dice(bot, message):
         rep_msg_id = message.reply_to_message.message.id
     await bot.send_dice(
     chat.id=message.chat.id,
-    emoji=EMOJI,
+    emoji=D_EMOJI,
     disable_web_page_preview=True,
     reply_to_message_id=rep_msg_id
     )
