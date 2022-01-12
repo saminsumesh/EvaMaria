181906b986686be30623b571621a88c4a44367d4
cracker <96644787+Mkproject8@users.noreply.github.com>
2022-01-12 08:20:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -91,7 +91,7 @@ This module only works for my admins
 • /users - <code>to get list of my users and ids.</code>
 • /chats - <code>to get list of the my chats and ids </code>
 • /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
+• /disable  -<code>do disable a chat.</code>
 • /ban  - <code>to ban a user.</code>
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>"""
